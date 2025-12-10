3bbc1ec572bb9a4fa4656bedfe14d6cbb7544f07
Shaun Belknap <shaun.belknap@gmail.com>
2025-12-09 20:39:38 -0600

    Added command map and callback function

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Pokedex >")
+		scanner.Scan()
+		text := scanner.Text()
+		cleanText := cleanInput(text)
+
+		if len(cleanText) == 0 {
+			continue
+		}
+
+		firstWord := cleanText[0]
+
+		if cmd, exists := commands[firstWord]; exists {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for name, cmd := range commands {
+		fmt.Printf("%s: %s\n", name, cmd.description)
+	}
+	return nil
 }
