82006ea29ef64283e24616db053651b0b301567c
Shaun Belknap <shaun.belknap@gmail.com>
2025-12-16 21:20:13 -0600

    Finished pokecache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -21,19 +21,40 @@ func NewCache(interval time.Duration) *Cache {
 		cacheEntries: make(map[string]cacheEntry),
 		mu: &sync.Mutex{},
 	}
+	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
 	defer c.mu.Unlock()
 	c.mu.Lock()
+	c.cacheEntries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
 	defer c.mu.Unlock()
 	c.mu.Lock()
-	return nil, false
+	if entry, ok := c.cacheEntries[key]; ok {
+		return entry.val, true
+	}else {
+		return nil, false
+	}
 }
 
 func (c *Cache) reapLoop(interval time.Duration) {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		<-ticker.C
+		now := time.Now()
+		for key, entry := range c.cacheEntries {
+			if now.Sub(entry.createdAt) > interval {
+				delete(c.cacheEntries, key)
+			}
+		}
+	}
 }
