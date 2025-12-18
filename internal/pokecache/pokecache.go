1fd519bf7115af0dff1f1e0743ebd6f09c783040
Shaun Belknap <shaun.belknap@gmail.com>
2025-12-18 00:06:21 -0600

    Retrying commit

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -5,21 +5,20 @@ import (
 	"time"
 )
 
-
 type Cache struct {
 	cacheEntries map[string]cacheEntry
-	mu *sync.Mutex
+	mu           *sync.Mutex
 }
 
 type cacheEntry struct {
 	createdAt time.Time
-	val []byte
+	val       []byte
 }
 
 func NewCache(interval time.Duration) *Cache {
 	c := &Cache{
 		cacheEntries: make(map[string]cacheEntry),
-		mu: &sync.Mutex{},
+		mu:           &sync.Mutex{},
 	}
 	go c.reapLoop(interval)
 	return c
